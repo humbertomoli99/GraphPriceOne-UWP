05c07345489cfddf7cf9d49f92bf391d29ef243d
HumbertoML <humberto_molinalopez@hotmail.com>
2021-10-26 10:51:47 -0500

    añadiendo BaseProductViewModel

---
+++ GraphPriceOne/ViewModels/BaseProductViewModel.cs
@@ -0,0 +1,59 @@
+﻿using GraphPriceOne.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphPriceOne.ViewModels
+{
+    public class BaseProductViewModel : INotifyPropertyChanged
+    {
+        private ProductInfo _productInfo;
+        public ProductInfo ProductInfo
+        {
+            get
+            {
+                return _productInfo;
+            }
+            set
+            {
+                _productInfo = value;
+                OnPropertyChanged();
+            }
+        }
+        bool isBusy = false;
+        public bool IsBusy
+        {
+            get
+            {
+                return isBusy;
+            }
+            set
+            {
+                SetProperty(ref isBusy, value);
+            }
+        }
+        protected bool SetProperty<T>(ref T backingStore,T value,[CallerMemberName] string propertyName = "", Action onChanged = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(backingStore, value))
+                return false;
+
+            backingStore = value;
+            onChanged?.Invoke();
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            var changed = PropertyChanged;
+            if (changed == null)
+                return;
+            changed.Invoke(this,new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+}
