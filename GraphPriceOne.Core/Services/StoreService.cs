1f6b7866139408837878121f233230c1b09ccb5b
HumbertoML <humberto_molinalopez@hotmail.com>
2021-11-11 17:26:06 -0600

    cambiando product por store

--- GraphPriceOne.Core/Services/StoreService.cs
+++ GraphPriceOne.Core/Services/StoreService.cs
@@ -11,7 +11,7 @@ namespace GraphPriceOne.Core.Services
     {
         public SQLiteAsyncConnection _database;
 
-        public async Task<bool> AddProductAsync(Store storeService)
+        public async Task<bool> AddStoreAsync(Store storeService)
         {
             if (storeService.ID_STORE > 0)
             {
@@ -24,23 +24,23 @@ namespace GraphPriceOne.Core.Services
             return await Task.FromResult(true);
         }
 
-        public async Task<bool> DeleteProductAsync(int id)
+        public async Task<bool> DeleteStoreAsync(int id)
         {
             await _database.DeleteAsync<Store>(id);
             return await Task.FromResult(true);
         }
 
-        public async Task<Store> GetProductAsync(int id)
+        public async Task<Store> GetStoreAsync(int id)
         {
             return await _database.Table<Store>().Where(p => p.ID_STORE == id).FirstOrDefaultAsync();
         }
 
-        public async Task<IEnumerable<Store>> GetProductsAsync()
+        public async Task<IEnumerable<Store>> GetStoresAsync()
         {
             return await Task.FromResult(await _database.Table<Store>().ToListAsync());
         }
 
-        public async Task<bool> UpdateProductAsync(Store storeService)
+        public async Task<bool> UpdateStoreAsync(Store storeService)
         {
             await _database.UpdateAsync(storeService);
             return await Task.FromResult(true);
