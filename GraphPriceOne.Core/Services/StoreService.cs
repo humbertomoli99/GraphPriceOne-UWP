306cc9e7c6d0277ef100eb7f6ad72137824cca5e
HumbertoML <humberto_molinalopez@hotmail.com>
2021-11-11 17:23:50 -0600

    creando servicio store service

---
+++ GraphPriceOne.Core/Services/StoreService.cs
@@ -0,0 +1,50 @@
+﻿using GraphPriceOne.Core.Models;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphPriceOne.Core.Services
+{
+    public class StoreService
+    {
+        public SQLiteAsyncConnection _database;
+
+        public async Task<bool> AddProductAsync(Store storeService)
+        {
+            if (storeService.ID_STORE > 0)
+            {
+                await _database.UpdateAsync(storeService);
+            }
+            else
+            {
+                await _database.InsertAsync(storeService);
+            }
+            return await Task.FromResult(true);
+        }
+
+        public async Task<bool> DeleteProductAsync(int id)
+        {
+            await _database.DeleteAsync<ProductInfo>(id);
+            return await Task.FromResult(true);
+        }
+
+        public async Task<ProductInfo> GetProductAsync(int id)
+        {
+            return await _database.Table<ProductInfo>().Where(p => p.ID_PRODUCT == id).FirstOrDefaultAsync();
+        }
+
+        public async Task<IEnumerable<ProductInfo>> GetProductsAsync()
+        {
+            return await Task.FromResult(await _database.Table<ProductInfo>().ToListAsync());
+        }
+
+        public async Task<bool> UpdateProductAsync(ProductInfo storeService)
+        {
+            await _database.UpdateAsync(storeService);
+            return await Task.FromResult(true);
+            //throw new NotImplementedException();
+        }
+    }
+}
