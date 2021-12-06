f1188206c8bfbf540a8da4f7095422e4a2672ee1
HumbertoML <humberto_molinalopez@hotmail.com>
2021-12-06 16:26:35 -0600

    Creando modelo productos de los botones de ocultar y mostrar para seleccionar o eliminar productos

---
+++ GraphPriceOne/Models/ProductsModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Windows.UI.Xaml;
+
+namespace GraphPriceOne.Models
+{
+    public class ProductsModel : PropertyChangedNotification
+    {
+        public Visibility IsCheckedAllVisibility
+        {
+            get { return GetValue(() => IsCheckedAllVisibility); }
+            set { SetValue(() => IsCheckedAllVisibility, value); }
+        }
+        public Visibility DeleteStoreVisibility
+        {
+            get { return GetValue(() => DeleteStoreVisibility); }
+            set { SetValue(() => DeleteStoreVisibility, value); }
+        }
+        public Visibility FirstProductVisibility
+        {
+            get { return GetValue(() => FirstProductVisibility); }
+            set { SetValue(() => FirstProductVisibility, value); }
+        }
+        public Visibility CommandBarVisibility
+        {
+            get { return GetValue(() => CommandBarVisibility); }
+            set { SetValue(() => CommandBarVisibility, value); }
+        }
+        public Visibility ListProductsVisibility { get; set; }
+
+        public ICommand SelectMultiple { get; set; }
+    }
+}
