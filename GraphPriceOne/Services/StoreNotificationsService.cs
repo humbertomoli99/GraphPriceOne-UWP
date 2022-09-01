578ee8bc90aa9ce5129139b241d7d3accb1df8d6
humberto <humberto_molinalopez@hotmail.com>
2022-08-31 23:56:14 -0500

    creando notificacion que al cliquearla te envia a el producto notificado

--- GraphPriceOne/Services/StoreNotificationsService.cs
+++ GraphPriceOne/Services/StoreNotificationsService.cs
@@ -1,17 +1,26 @@
 ﻿using GraphPriceOne.Activation;
+using GraphPriceOne.ViewModels;
+using GraphPriceOne.Views;
 using Microsoft.Services.Store.Engagement;
+using Microsoft.Toolkit.Uwp.Notifications;
 using System;
+using System.Collections.Specialized;
+using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 using Windows.ApplicationModel.Activation;
+using Windows.Foundation.Collections;
 
 namespace GraphPriceOne.Services
 {
     internal class StoreNotificationsService : ActivationHandler<ToastNotificationActivatedEventArgs>
     {
+        private ProductDetailsViewModel selectors;
         public async Task InitializeAsync()
         {
             try
             {
+                selectors = new ProductDetailsViewModel();
                 var engagementManager = StoreServicesEngagementManager.GetDefault();
                 await engagementManager.RegisterNotificationChannelAsync();
             }
@@ -29,6 +38,21 @@ namespace GraphPriceOne.Services
             string originalArgs = engagementManager.ParseArgumentsAndTrackAppLaunch(toastActivationArgs.Argument);
 
             //// Use the originalArgs variable to access the original arguments passed to the app.
+            ///
+
+            // Obtain the arguments from the notification
+            ToastArguments argumentsParsed = ToastArguments.Parse(toastActivationArgs.Argument);
+
+            // Obtain any user input (text boxes, menu selections) from the notification
+            ValueSet userInput = toastActivationArgs.UserInput;
+
+            int ProductId;
+            if (argumentsParsed["Action"] == "viewProduct")
+            {
+                ProductId = Int32.Parse(argumentsParsed["ProductId"]);
+                selectors.SelectedProduct = ProductId;
+                NavigationService.Navigate(typeof(ProductDetailsPage));
+            }
 
             await Task.CompletedTask;
         }
--- GraphPriceOne/Views/ExportPage.xaml.cs
+++ GraphPriceOne/Views/ExportPage.xaml.cs
@@ -1,6 +1,5 @@
-﻿using GraphPriceOne.Library;
-using GraphPriceOne.ViewModels;
-
+﻿using GraphPriceOne.ViewModels;
+using Microsoft.Toolkit.Uwp.Notifications;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
 
@@ -26,7 +25,13 @@ namespace GraphPriceOne.Views
 
         private void Button_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
         {
-            ExportData.ExportDataCsv();
+            //ExportData.ExportDataCsv();
+            new ToastContentBuilder()
+    .AddArgument("Action", "viewProduct")
+    .AddArgument("ProductId", 4)
+    .AddText("Tu mejor tarjeta de video esta aqui")
+    .AddText("Check this out, The Enchantments in Washington!")
+    .Show();
         }
     }
 }
